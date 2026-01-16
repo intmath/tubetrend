77290ff29efd186d5b5d8fb4076cf44c6e021d9c
intmath <gaikai@naver.com>
2026-01-16 09:59:54 +0900

    Update index.js
    아이폰에서 UI 동일하게 수정

--- src/index.js
+++ src/index.js
@@ -673,7 +673,7 @@ const HTML_CONTENT = `
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=1280, user-scalable=yes">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" crossorigin href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
